9010b7b874c7bd22b342835eab56b3cb20f224b3
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-08-26 14:39:25 +0200

    simple exercise grow

--- src/Grow.java
+++ src/Grow.java
@@ -1,8 +1,16 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Grow {
     public static void main(String[] args) {
-
+           grow( new int[]{5,3,4});
     }
-    public static int grow(int[]x){
+    public static int grow(int[] x){
+      int quotiet =  1;
+        for (int i = 0; i < x.length ; i++) {
+            quotiet*=x[i];
+        }
+        System.out.println(quotiet);
         return 0;
     }
 }
